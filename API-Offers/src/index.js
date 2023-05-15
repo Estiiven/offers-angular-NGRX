341bd19bf0e1d1f1551dcd70f7811a4612276401
Estiven Gómez Castaño <segomezc2@gmail.com>
2023-05-15 12:29:18 -0500

    api headers and other details

--- API-Offers/src/index.js
+++ API-Offers/src/index.js
@@ -6,11 +6,19 @@ const morgan = require('morgan');
 app.set('port', process.env.PORT || 3000)
 app.set('json spaces', 2);
 
+//Headers
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 //middlewares
-app.use(morgan('dev')); 
+app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
-  
 //routes
 //app.use('/api/main',require('./routes/index'));
 app.use('/api/ofertas',require('./routes/offers'));
